cdfcedd0caa53aa62658a3d4c5d2874ed2506e79
joshu <joshuata1@aol.com>
2020-05-20 16:40:15 +0100

    more tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,28 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 
-# Create your tests here.
+from blog.views import post_list
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, post_list)
+
+    def test_uses_post_list(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'blog/post_list.html')
+
+    def test_uses_post_detail(self):
+            response = self.client.get('/')
+            self.assertTemplateUsed(response, 'blog/post_list.html')
+
+    def test_uses_post_new(self):
+            response = self.client.get('/')
+            self.assertTemplateUsed(response, 'blog/post_list.html')
+
+    def test_uses_post_edit(self):
+            response = self.client.get('/')
+            self.assertTemplateUsed(response, 'blog/post_list.html')
